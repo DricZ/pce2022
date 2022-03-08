0fdb1e6d9c7d3208fbf10c10999df3766a72c6f7
ToniAriyanto <96300795+ToniAriyanto@users.noreply.github.com>
2022-03-08 23:18:54 +0800

    Update test_map.php

--- 2021/bc/rally_games/test_map.php
+++ 2021/bc/rally_games/test_map.php
@@ -518,11 +518,10 @@
                     <path d="M217 28.098v455.804l142-42.597V70.697zm159.938 26.88l.062 2.327V87h16V55zM119 55v117.27h18V73h62V55zm258 50v16h16v-16zm0 34v236h16V139zm-240 58.727V233H41v46h96v35.273L195.273 256zM244 232c6.627 0 12 10.745 12 24s-5.373 24-12 24-12-10.745-12-24 5.373-24 12-24zM137 339.73h-18V448h18zM377 393v14h16v-14zm0 32v23h16v-23zM32 471v18h167v-18zm290.652 0l-60 18H480v-18z"></path>
                 </g>
             </svg>
-    
     </a>
         </div>
         <div class="row icon-navBar">
-            <div class="col-md-3"><img src="assets/logo_zoom_out.png"onclick="_zoomOut()" alt="" style="width:50px;"></div>
+            <div class="col-md-3"><img src="assets/treasure.png"onclick="_zoomOut()" alt="" style="width:50px;"></div>
         </div>
         
         
