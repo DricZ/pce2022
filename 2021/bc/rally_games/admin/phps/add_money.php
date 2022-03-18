6b84fcd5e0906cd710557cebe7c837e3547dd86d
IaN99999 <96299893+IaN99999@users.noreply.github.com>
2022-03-18 17:56:35 +0700

    Update add_money.php

--- 2021/bc/rally_games/admin/phps/add_money.php
+++ 2021/bc/rally_games/admin/phps/add_money.php
@@ -4,17 +4,25 @@ require "connect.php";
 if (isset($_POST)) {
     $username = $_POST['team'];
     $money = $_POST['money'];
-    $keterangan = $_POST['keterangan'] . "(Menggunakan <b>X2 Scocial Credits</b>)";
+    define('TIMEZONE', 'Asia/Jakarta');
+    date_default_timezone_set(TIMEZONE);
+    $timestamp = date("d-m-Y H:i:s");
 
     $sql_cek =  "SELECT * FROM team WHERE username = ? AND x2_earning != 0";
     $stmt_cek = $pdo->prepare($sql_cek);
     $stmt_cek->execute([$username]);
 
     if ($stmt_cek->rowCount() == 0) {
+        $keterangan = $_POST['keterangan'];
         $addMoneysql = "UPDATE `team` SET `money` = `money` + ? WHERE username = ?";
         $addMoneystmt = $pdo->prepare($addMoneysql);
         $addMoneystmt->execute([$money, $username]);
+
+        $historysql = "INSERT INTO `history_add_money`(`id`, `username`, `money_value`, `added_on`, `added_by`, `keterangan`) VALUES (NULL, ?, ?, ?, ?, ?)";
+        $historystmt = $pdo->prepare($historysql);
+        $historystmt->execute([$username, $money, $timestamp, $_SESSION['namaAdmin'], $keterangan]);
     } else {
+        $keterangan = $_POST['keterangan'] . "(Menggunakan <b>X2 Scocial Credits</b>)";
         $addMoneysql = "UPDATE `team` SET `money` = `money` + (?*2) WHERE username = ?";
         $addMoneystmt = $pdo->prepare($addMoneysql);
         $addMoneystmt->execute([$money, $username]);
@@ -22,15 +30,14 @@ if (isset($_POST)) {
         $matikanSkill = "UPDATE `team` SET `x2_earning` = 0 WHERE username = ?";
         $matikanStmt = $pdo->prepare($matikanSkill);
         $matikanStmt->execute([$username]);
+        $historysql = "INSERT INTO `history_add_money`(`id`, `username`, `money_value`, `added_on`, `added_by`, `keterangan`) VALUES (NULL, ?, ?, ?, ?, ?)";
+        $historystmt = $pdo->prepare($historysql);
+        $historystmt->execute([$username, $money, $timestamp, $_SESSION['namaAdmin'], $keterangan]);
     }
 
-    define('TIMEZONE', 'Asia/Jakarta');
-    date_default_timezone_set(TIMEZONE);
-    $timestamp = date("d-m-Y H:i:s");
+    
 
-    $historysql = "INSERT INTO `history_add_money`(`id`, `username`, `money_value`, `added_on`, `added_by`, `keterangan`) VALUES (NULL, ?, ?, ?, ?, ?)";
-    $historystmt = $pdo->prepare($historysql);
-    $historystmt->execute([$username, $money, $timestamp, $_SESSION['namaAdmin'], $keterangan]);
+    
 
     header("Location: ../give_money.php?stat=1");
     exit();
