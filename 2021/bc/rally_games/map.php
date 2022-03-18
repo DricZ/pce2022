7f7616d4b46dc69699c59afb370f5ecd148a727c
IaN99999 <96299893+IaN99999@users.noreply.github.com>
2022-03-18 17:05:07 +0700

    Update map.php

--- 2021/bc/rally_games/map.php
+++ 2021/bc/rally_games/map.php
@@ -2538,40 +2538,22 @@
     </div>
 </div>
 
+
 <div class="modal fade" id="modal_bencana">
     <div class="modal-dialog">
         <div class="modal-content">
-            <!-- Modal Header -->
-            <div class="modal-header" style='font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";'>
-                <h1 class="modal-title"style ='margin-left: auto;'>Terjadi Bencana!</h1>
-                <button style="margin-top: -7px;" type="button" class="close" data-dismiss="modal">&times;</button>
-                
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h1 class="modal-title">Terjadi Bencana!</h1>
+                    <p>Bencana akan menurunkan tingkatan dari jembatan anda bahkan sampai hancur </p>
+                    <center><img src="" alt="" style="width: 50%; " id = 'imgbencana'></center>
+                    <p>Untuk mempelajari lebih lanjut silakan ke :</p>
+                    <a href="#" class="btn btn-info" role="button" style="width: inherit;">Info</a>
+                    
             </div>
 
-            <!-- Modal body -->
-            <div class="modal-body"
-                style='font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";'>
-                <div class="container">
-                    <div class="row" style="width:100%; margin-top:-2%; margin-left:-15px">
-
-                        <!-- Jembatan Beton -->
-                        
-                        <div class="col-12 jembatan hidden" id="upbeton">
-
-                            <p>Bencana akan menurunkan tingkatan dari jembatan anda bahkan sampai hancur </p>
-                            <center><img src="assets/image/angin beliung.png" alt="" style="width: 50%;"></center>
-                            <p>Untuk mempelajari lebih lanjut silakan ke :</p>
-                            <a href="#" class="btn btn-info" role="button" style="width: inherit;">Info</a>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Modal footer -->
-                <div class="modal-footer">
-                    <p>
-                      <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    </p>
-                </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
