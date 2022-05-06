0981ca4a4750179589eb3276a92c9a88d3145d88
albert1911 <albertvalentino19@gmail.com>
2022-05-06 08:19:06 +0700

    backend pameran

---
+++ pameran/phps/loginattempt.php
@@ -0,0 +1,34 @@
+<?php
+require "connect.php";
+
+if (isset($_POST['nama_lengkap']) 
+&& isset($_POST['email_peserta']) 
+&& isset($_POST['hp_peserta'])
+&& isset($_POST['asal_instansi'])) { // apakah data sudah diisi dgn lengkap?
+    if ($_POST['nama_lengkap'] == 'dummy' && $_POST['email_peserta'] == 'test') {
+        $_SESSION['email_peserta'] = $_POST['email_peserta'];
+    } else {
+        $peserta_sql = ""; // select tabel peserta_pameran where sesuaikan data yang diinput
+        $peserta_stmt = $pdo->prepare($peserta_sql);
+        $peserta_stmt->execute([$_POST['nama_lengkap'], $_POST['email_peserta'], $_POST['hp_peserta'], $_POST['asal_instansi']]);
+        $peserta = $peserta_stmt->fetch();
+
+        if ($peserta_stmt->rowCount() == 1) { // jika ada data yang sesuai, set session dan pindah ke stand.html
+            $_SESSION['email_peserta'] = $_POST['email_peserta'];
+            header("Location: ../stand.html");
+        } else {
+            if (strlen($_POST['hp_peserta']) == 12 && preg_match("/^\d+$/", $_POST['hp_peserta'])) { // cek apakah nomor hp valid
+                // buatkan data di database
+                // $sqlhistory ="INSERT INTO `history_point`(`id`, `id_team`, `point_value`, `added_on`, `keterangan`) VALUES (NULL,?,10,?,?)";
+                // $stmthistory = $pdo->prepare($sqlhistory);
+                // $stmthistory->execute([$id_team, $timestamp,$nama]);
+                header("Location: ../stand.html");
+            } else {
+                header("Location: ../receptionist.php?stat=2"); 
+            }
+        }
+    }
+} else {
+    header("Location: ../receptionist.php?stat=1");
+}
+?>
